88d621f36d8dbfe28a8cecff5b95a3187ad22fb5
misael.bueno <misael.bueno@meta.com.br>
2023-11-29 15:29:31 -0300

    testes de userCases

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user.admin) {
+      throw new Error("Forbidden!");
+    }
+
+    const usersList = this.usersRepository.list();
+
+    return usersList;
   }
 }
 
